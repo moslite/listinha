241a5fbdf94b4eb8b0df33a327164771816db889
Murilo Souza <moslite@hotmail.com>
2023-02-20 09:43:08 -0300

    Criando tela de tasks

--- app/lib/src/home/home_page.dart
+++ app/lib/src/home/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:listinha/src/home/widgets/task_card.dart';
+import 'package:listinha/src/shared/services/realm/models/task_model.dart';
+import 'package:realm/realm.dart';
 
 import '../shared/widgets/user_image_button.dart';
 import 'widgets/custom_drawer.dart';
@@ -27,31 +30,65 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       body: Center(
-        child: Column(
+        child: Stack(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(8),
-              child: SegmentedButton(
-                segments: const [
-                  ButtonSegment(
-                    value: 0,
-                    label: Text('Todos'),
-                  ),
-                  ButtonSegment(
-                    value: 1,
-                    label: Text('Pendentes'),
-                  ),
-                  ButtonSegment(
-                    value: 2,
-                    label: Text('Concluídas'),
-                  ),
-                  ButtonSegment(
-                    value: 3,
-                    label: Text('Desativadas'),
-                  ),
-                ],
-                selected: const {0},
-                onSelectionChanged: (values) {},
+            ListView.separated(
+              padding: const EdgeInsets.only(
+                top: 50,
+                left: 30,
+                right: 30,
+                bottom: 200,
+              ),
+              separatorBuilder: (_, index) {
+                return const SizedBox(
+                  height: 10,
+                );
+              },
+              itemCount: 100,
+              itemBuilder: (_, index) {
+                final board = TaskBoard(
+                  Uuid.v4(),
+                  'Nova lista de tarefas 1',
+/*                   tasks: [
+                    Task(Uuid.v4(), '', completed: true),
+                    Task(Uuid.v4(), '', completed: true),
+                    Task(Uuid.v4(), '', completed: true),
+                    Task(Uuid.v4(), '', completed: true),
+                  ], */
+                  enable: true,
+                );
+                return TaskCard(
+                  board: board,
+                  height: 140,
+                );
+              },
+            ),
+            Align(
+              alignment: Alignment.topCenter,
+              child: Padding(
+                padding: const EdgeInsets.all(8),
+                child: SegmentedButton(
+                  segments: const [
+                    ButtonSegment(
+                      value: 0,
+                      label: Text('Todos'),
+                    ),
+                    ButtonSegment(
+                      value: 1,
+                      label: Text('Pendentes'),
+                    ),
+                    ButtonSegment(
+                      value: 2,
+                      label: Text('Concluídas'),
+                    ),
+                    ButtonSegment(
+                      value: 3,
+                      label: Text('Desativadas'),
+                    ),
+                  ],
+                  selected: const {0},
+                  onSelectionChanged: (values) {},
+                ),
               ),
             )
           ],
--- app/lib/src/shared/services/realm/realm_config.dart
+++ app/lib/src/shared/services/realm/realm_config.dart
@@ -1,8 +1,13 @@
 import 'package:listinha/src/shared/services/realm/models/configuration_model.dart';
+import 'package:listinha/src/shared/services/realm/models/task_model.dart';
 import 'package:realm/realm.dart';
 
 var config = Configuration.local(
-  [ConfigurationModel.schema],
+  [
+    ConfigurationModel.schema,
+    Task.schema,
+    TaskBoard.schema,
+  ],
   initialDataCallback: (realm) {
     realm.add(ConfigurationModel('system'));
   },
